2a7b92aae9a0fca0b2fbeda521a827aab53d954a
PluieElectrique <41453973+PluieElectrique@users.noreply.github.com>
2020-12-23 07:02:39 -0800

    Add script

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # mediafire-dl
-Download files, folders, and metadata from Mediafire
+
+Download files, folders, and metadata from Mediafire.
+
+## Setup
+
+Requires Python 3. (Tested on 3.9.1.)
+
+Install `requests` and `tqdm`, e.g. with `pip install -r requirements.txt`.
+
+## Usage
+
+Put your file keys (quickkeys) and folder keys in a JSON file like so:
+```json
+{
+    "file": [
+        "[quickkey 1]",
+        "[quickkey 2]"
+    ],
+    "folder": [
+        "[folderkey 1]",
+        "[folderkey 2]"
+    ]
+}
+```
+(If you're just downloading files, you can leave out `folder`, and vice versa.)
+
+Run the tool with:
+```
+python mediafire_dl.py [--metadata-only] [--indent INDENT] input out_dir
+```
+Where `input` is the JSON file from above, and `out_dir` is where you want to save everything. Pass `--metadata-only` to only fetch metadata or `--indent` to change the JSON indent.
+
+The structure of the output directory will look like this:
+```
+out_dir/
+  mfdl_summary_[TIMESTAMP].json
+  some owner/
+    examplefile_[QUICKKEY].txt
+    examplefile_[QUICKKEY].txt.json
+    a folder_[FOLDERKEY]/
+      otherfile_[QUICKKEY].pdf
+      otherfile_[QUICKKEY].pdf.json
+      ...
+    some folder_[FOLDERKEY].json
+    ...
+  another owner/
+    ...
+```
+The `mfdl_summary_[TIMESTAMP].json` file contains the skipped file/folder keys (for which the API returned nothing) and the deleted file keys (for which the API returned data, but the actual file is gone).
+
+## Notes
+
+* When resuming a download job, already downloaded files will not be redownloaded and partially downloaded files will be resumed. However, all metadata will be redownloaded.
+* Folder avatars are not saved.
+* Upload countries are not included if `--metadata-only` is passed, as doing so requires scraping the file download page.
+* The API does not provide a unique ID for owners. So, if two owners have the same name, their files will share the same directory. (At the very least, there’s no chance of files/folders overwriting each other because all file/folder names include their unique key.)
+
+## Legal
+
+This program is licensed under the MIT License. See the `LICENSE` file for more information.
---
+++ mediafire_dl.py
@@ -0,0 +1,383 @@
+import argparse
+from collections import defaultdict
+from datetime import datetime
+import json
+import logging
+import os
+import re
+
+import requests
+from tqdm import tqdm, trange
+
+
+logger = logging.getLogger(__name__)
+
+ASCII_CONTROL = re.compile(r"[\x00-\x1f\x7f]")
+FILENAME_FORBIDDEN = re.compile(r'[\\/:*?"<>|]')
+LEADING_DASH_DOT = re.compile(r"_*[-.]")
+TRAILING_DOTS = re.compile(r"\.+$")
+
+
+def try_mkdir(path):
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        pass
+
+
+def sanitize_filename(filename, max_bytes=255):
+    filename = ASCII_CONTROL.sub("", filename)
+    # The forbidden characters are common enough that we replace them with an
+    # underscore to show that a character was replaced.
+    filename = FILENAME_FORBIDDEN.sub("_", filename)
+    filename = filename.strip()
+    # Prepend an underscore to files starting with a dot or dash--this prevents
+    # the file from being hidden or being interpreted as a flag on Unix-likes.
+    # We need _* to ensure that prepending an underscore doesn't conflict with
+    # any existing files.
+    if LEADING_DASH_DOT.match(filename):
+        filename = "_" + filename
+    # Limit the filename to max_bytes at most. We assume UTF-8.
+    encoded = filename.encode()
+    if len(encoded) > max_bytes:
+        filename = encoded[:max_bytes].decode(errors="ignore")
+        # Truncation might have exposed trailing spaces, so we strip again.
+        filename = filename.strip()
+    return filename
+
+
+def get_clean_filename(qk, name):
+    # Windows doesn't like files that end with a period
+    name = TRAILING_DOTS.sub("_", name)
+    root, ext = os.path.splitext(name)
+    # We ignore the extension if it contains forbidden characters.
+    if ASCII_CONTROL.search(ext) or FILENAME_FORBIDDEN.search(ext):
+        root, ext = name, ""
+
+    # Leave room for underscore, 15-character quickkey, extension, and ".json"
+    sanitized = sanitize_filename(root, max_bytes=255 - 1 - 15 - len(ext) - 5)
+    return f"{sanitized}_{qk}{ext}"
+
+
+def get_clean_foldername(fk, name):
+    # Leave room for underscore, 13-character folderkey, and ".json"
+    sanitized = sanitize_filename(name, max_bytes=255 - 1 - 13 - 5)
+    return f"{sanitized}_{fk}"
+
+
+class MediafireError(Exception):
+    pass
+
+
+class MediafireDownloader:
+    MEDIAFIRE_API_BASE = "https://www.mediafire.com/api/1.5/"
+    QUICKKEY_RE = re.compile(r"[a-z0-9]{11}|[a-z0-9]{15}")
+    DL_URL_RE = re.compile(r"https://download\d+\.mediafire\.com/")
+    DL_URL_SCRAPE_RE = re.compile(
+        r'aria-label="Download file"\n +href="([^"]+)"\n +id="downloadButton"'
+    )
+    UPLOAD_COUNTRY_SCRAPE_RE = re.compile(
+        r'<div class="lazyload DLExtraInfo-sectionGraphic flag" data-lazyclass="flag-(..)">'
+    )
+    MAX_REDIRECTS = requests.models.DEFAULT_REDIRECT_LIMIT
+
+    def __init__(self):
+        self.s = requests.Session()
+
+    def mf_api(self, method, params):
+        params["response_format"] = "json"
+        r = self.s.post(self.MEDIAFIRE_API_BASE + method + ".php", params=params)
+        resp = r.json()["response"]
+        if resp["result"] == "Success":
+            return resp
+        else:
+            raise MediafireError((resp["error"], resp["message"]))
+
+    # We set a default chunk size just below the max of 500 to be safe
+    def get_file_info(self, quickkeys, chunk_size=450):
+        if not quickkeys:
+            return [], []
+
+        def _validate_quickkey(k):
+            valid = self.QUICKKEY_RE.fullmatch(k)
+            if not valid:
+                logger.warning("f{k} is not a valid quickkey")
+            return valid
+
+        quickkeys = list(filter(_validate_quickkey, quickkeys))
+        file_info = []
+        skipped = []
+        for i in trange(
+            0,
+            len(quickkeys),
+            chunk_size,
+            unit="req",
+            desc="Get file info",
+        ):
+            chunk = quickkeys[i : i + chunk_size]
+            try:
+                resp = self.mf_api("file/get_info", {"quick_key": ",".join(chunk)})
+                if len(chunk) == 1:
+                    file_info.append(resp["file_info"])
+                else:
+                    # There is an "s" when there is more than one quickkey
+                    file_info.extend(resp["file_infos"])
+                skipped.extend(filter(None, resp.get("skipped", "").split(",")))
+            except MediafireError as err:
+                if err.args[0][0] != 110:
+                    logger.error(f"Failed to get info for quickkeys {chunk}: {err}")
+                skipped.extend(chunk)
+
+        return file_info, skipped
+
+    # The API call fails if even one folderkey is invalid, so we only request
+    # info about one folder at a time. We don't validate folderkey because it
+    # seems like custom folderkeys can be any string. We don't support the
+    # `details` parameter because `get_content` is far more useful.
+    def get_folder_info(self, folderkey):
+        try:
+            return self.mf_api("folder/get_info", params={"folder_key": folderkey})[
+                "folder_info"
+            ]
+        except MediafireError as err:
+            if err.args[0][0] != 112:
+                logger.error(f"Failed to get info for folder {folderkey}: {err}")
+
+    def get_folder_contents(
+        self, folderkey, content_type="folders", owner_name=None, chunk_size=1000
+    ):
+        if content_type not in ["folders", "files"]:
+            raise ValueError("content_type must be 'folders' or 'files'")
+        elif not (100 <= chunk_size <= 1000):
+            raise ValueError("chunk_size must be between 100 and 1000, inclusive")
+
+        params = {
+            "folder_key": folderkey,
+            "chunk_size": chunk_size,
+            "content_type": content_type,
+        }
+        contents = []
+        chunk = 1
+        while True:
+            params["chunk"] = chunk
+            try:
+                resp = self.mf_api("folder/get_content", params=params)
+            except MediafireError:
+                logger.error(f"Failed to get chunk {chunk} of folder {folderkey}")
+                return contents
+            chunk += 1
+
+            content = resp["folder_content"]
+            if owner_name:
+                for c in content[content_type]:
+                    # `get_content` does not return owner_name, so we hackily
+                    # add it in
+                    c["owner_name"] = owner_name
+
+            contents.extend(content[content_type])
+            if content["more_chunks"] == "no":
+                break
+
+        return contents
+
+    def scrape_download_page(self, url):
+        r = self.s.get(url, allow_redirects=False)
+        redirects = 0
+        while r.next:
+            redirects += 1
+            if redirects > self.MAX_REDIRECTS:
+                raise Exception("Exceeded {self.MAX_REDIRECTS} redirects")
+
+            location = r.headers["Location"]
+            # Some quickkeys redirect to a direct download
+            if self.DL_URL_RE.match(location):
+                return location, None
+
+            r = self.s.send(r.next, allow_redirects=False)
+
+        download_url = self.DL_URL_SCRAPE_RE.search(r.text)
+        # The URL will be missing if the file no longer exists, was removed for
+        # violating TOS, etc.
+        if download_url:
+            upload_country = self.UPLOAD_COUNTRY_SCRAPE_RE.search(r.text)
+            upload_country = upload_country.group(1) if upload_country else None
+            return download_url.group(1), upload_country
+        else:
+            return None, None
+
+    def download_file(self, url, path, file_size=None, chunk_size=16384):
+        if file_size is None:
+            file_size = int(self.s.head(url).headers["Content-Length"])
+        start_byte = os.path.getsize(path) if os.path.exists(path) else 0
+
+        if file_size == start_byte:
+            return
+
+        download_url, upload_country = self.scrape_download_page(url)
+        if not download_url:
+            logger.warning(f"No download URL found for {url}")
+            return
+
+        desc = os.path.basename(path)
+        if len(desc) > 60:
+            desc = desc[:28] + "<..>" + desc[-28:]
+        elif len(desc) < 60:
+            desc = f"{desc:<60}"
+
+        with open(path, "ab") as f, self.s.get(
+            download_url, headers={"Range": f"bytes={start_byte}-"}, stream=True
+        ) as r, tqdm(
+            initial=start_byte,
+            total=file_size,
+            desc=desc,
+            unit="B",
+            unit_scale=True,
+        ) as pbar:
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                # NOTE: This is necessary to filter out "keep-alive" chunks?
+                # But the requests docs don't mention this at all.
+                if chunk:
+                    f.write(chunk)
+                    pbar.update(len(chunk))
+
+        return upload_country
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download files, folders, and metadata from Mediafire"
+    )
+    parser.add_argument(
+        "input",
+        help='Input JSON: {"file": [list of quickkeys], "folder": [list of folderkeys]}',
+    )
+    parser.add_argument("out_dir", help="Output directory")
+    parser.add_argument(
+        "--metadata-only", help="Only get metadata", action="store_true"
+    )
+    parser.add_argument(
+        "--indent", help="JSON indent (default: %(default)s)", type=int, default=None
+    )
+    args = parser.parse_args()
+
+    with open(args.input) as f:
+        input_keys = json.load(f)
+
+    os.makedirs(args.out_dir, exist_ok=True)
+    mfdl = MediafireDownloader()
+
+    # File and folders which don’t return an API response
+    skipped = []
+    # Files which have a "delete_date"
+    deleted = []
+
+    pbar = tqdm(desc="Get folder info", unit="")
+    folders = defaultdict(lambda: {"is_child": False, "children": []})
+    folderkeys_seen = set()
+    folderkeys_queue = set(input_keys.get("folder", []))
+    while folderkeys_queue:
+        fk = folderkeys_queue.pop()
+        folderkeys_seen.add(fk)
+
+        if fk in folders:
+            folder = folders[fk]
+        else:
+            info = mfdl.get_folder_info(fk)
+            if info:
+                folder = folders[fk]
+                folder["info"] = info
+            else:
+                skipped.append(fk)
+                continue
+
+        if int(folder["info"]["folder_count"]) > 0:
+            child_folders = mfdl.get_folder_contents(
+                fk, content_type="folders", owner_name=folder["info"]["owner_name"]
+            )
+            folder["children"].extend(c["folderkey"] for c in child_folders)
+
+            for child_info in child_folders:
+                cfk = child_info["folderkey"]
+                folders[cfk].update(info=child_info, is_child=True)
+                if (cfk not in folderkeys_seen) and (cfk not in folderkeys_queue):
+                    folderkeys_queue.add(cfk)
+
+        pbar.update(1)
+
+    pbar.close()
+
+    quickkeys_seen = set()
+
+    def process_file(info, path):
+        qk = info["quickkey"]
+        quickkeys_seen.add(qk)
+
+        name = get_clean_filename(qk, info["filename"])
+
+        if "delete_date" in info:
+            deleted.append(qk)
+        elif not args.metadata_only:
+            upload_country = mfdl.download_file(
+                info["links"]["normal_download"],
+                os.path.join(path, name),
+                file_size=int(info["size"]),
+            )
+            if upload_country:
+                info["upload_country"] = upload_country
+
+        with open(os.path.join(path, name + ".json"), "w") as f:
+            json.dump(info, f, indent=args.indent)
+
+    def process_folder(fk, path, pbar):
+        folder = folders[fk]
+        info = folder["info"]
+        logger.info(f"Processing folder {fk}: {info['name']}")
+        name = get_clean_foldername(fk, info["name"])
+        folder_path = os.path.join(path, name)
+        try_mkdir(folder_path)
+
+        with open(os.path.join(path, name + ".json"), "w") as f:
+            json.dump(info, f, indent=args.indent)
+
+        if int(info["file_count"]) > 0:
+            for child_info in mfdl.get_folder_contents(
+                fk, content_type="files", owner_name=info["owner_name"]
+            ):
+                process_file(child_info, folder_path)
+
+        for cfk in folder["children"]:
+            process_folder(cfk, folder_path, pbar)
+
+        pbar.update(1)
+
+    with tqdm(desc="Process folders", total=len(folders), unit="") as pbar:
+        for fk, folder in folders.items():
+            if not folder["is_child"]:
+                path = os.path.join(
+                    args.out_dir, sanitize_filename(folder["info"]["owner_name"])
+                )
+                try_mkdir(path)
+                process_folder(fk, path, pbar)
+
+    quickkeys = list(set(input_keys.get("file", [])) - quickkeys_seen)
+    file_infos, file_skipped = mfdl.get_file_info(quickkeys)
+    skipped.extend(file_skipped)
+    for file_info in file_infos:
+        path = os.path.join(args.out_dir, sanitize_filename(file_info["owner_name"]))
+        try_mkdir(path)
+        process_file(file_info, path)
+
+    # Not UTC or ISO 8601, but it's readable and filename-safe
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    summary_path = os.path.join(args.out_dir, f"mfdl_summary_{now}.json")
+    with open(summary_path, "w") as f:
+        json.dump(
+            {
+                "skipped": skipped,
+                "deleted": deleted,
+            },
+            f,
+            indent=args.indent,
+        )
+
+    print(f"Summary JSON written to: {summary_path}")
